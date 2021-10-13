4d442be4257e13e523a9d57f0f7321050d1674d1
Akshat Trivedi <akshattrivedi7901@gmail.com>
2021-10-13 17:27:42 +0530

    SKILLS

--- js/main.js
+++ js/main.js
@@ -28,3 +28,28 @@ function linkAction() {
   navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/* ================ SKILLS BAR ================= */
+const skillsNumber = document.querySelectorAll(".skills__number");
+const skillsPercentage = document.querySelectorAll(".skills__percentage");
+for (let i = 0; i < skillsNumber.length; i++) {
+  const element = skillsNumber[i];
+  skillsPercentage[i].style.width = element.innerHTML;
+}
+
+/* ================ SKILLS ACCORDION ================= */
+const skillsContent = document.querySelectorAll(".skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (let i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+
+skillsHeader.forEach((el) => el.addEventListener("click", toggleSkills));
